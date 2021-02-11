03bf1602ae50e59065d88358648a51c048824614
Erik Scow <erikscow@gmail.com>
2021-02-11 13:16:48 -0800

    created database access code

---
+++ backend/models/dailyModel.js
@@ -0,0 +1,45 @@
+const pool = require('../db-config')
+
+const findById = async (id) => {
+    const day = await pool.query("SELECT * FROM daily_data WHERE id=$1", [id])
+    return day.rows[0]
+}
+
+const findByDate = async (day) => {
+    const day = await pool.query("SELECT * FROM daily_data WHERE date=$1", [day])
+    return day.rows[0]
+}
+
+const findAllByUser = async (user_id) => {
+    const allDailyData = await pool.query("SELECT * FROM daily_data WHERE user_id=$1", [user_id])
+    return allDailyData.rows[0]
+}
+
+const add = async (dayInfo) => {
+    const newDay = await pool.query(
+        "INSERT INTO daily_data(date, calorie_total, weight, bmr, calorie_suggestion, positive, user_id) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *",
+        [dayInfo.date, dayInfo.calorie_total, dayInfo.weight, dayInfo.bmr, dayInfo.calorie_suggestion, dayInfo.positive, dayInfo.user_id])
+    return newDay.rows[0]
+}
+
+const update = async (newInfo, id) => {
+    const original = await findById(id)
+    const updated = { ...original, ...newInfo }
+    const updatedDay = await pool.query(
+        "UPDATE daily_data SET date=$1 calorie_total=$2 weight=$3 bmr=$4 calorie_suggestion=$5 positive=$6 user_id=$7 RETURNING *",
+        [updated.date, updated.calorie_total, updated.weight, updated.bmr, updated.calorie_suggestion, updated.positive, updated.user_id])
+    return updatedDay.rows[0]
+}
+
+const remove = async (id) => {
+    await pool.query("DELETE * FROM daily_data WHERE id = $1", [id])
+}
+
+module.exports = {
+    findById,
+    findByDate,
+    findAllByUser,
+    add,
+    remove,
+    update
+}
---
+++ backend/models/eventModel.js
@@ -0,0 +1,29 @@
+const pool = require('../db-config')
+
+const findById = async (id) => {
+    const event = await pool.query("SELECT * FROM calorie_events WHERE id=$1", [id])
+    return event.rows[0]
+}
+
+const findAllByDay = async (day_id) => {
+    const allEvents = await pool.query("SELECT * FROM calorie_events WHERE day_id=$1", [day_id])
+    return allEvents.rows[0]
+}
+
+const add = async (eventInfo) => {
+    const newDay = pool.query(
+        "INSERT INTO calorie_events(note, magnitude, positive, day_id) VALUES($1, $2, $3, $4) RETURNING *",
+        [eventInfo.note, eventInfo.magnitude, eventInfo.positive, eventInfo.day_id])
+    return newDay.rows[0]
+}
+
+const remove = async (id) => {
+    pool.query("DELETE * FROM calorie_events WHERE id = $1", [id])
+}
+
+module.exports = {
+    findById,
+    findAllByDay,
+    add,
+    remove,
+}
