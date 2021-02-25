2a64fda941b033c79b40b68dbeabbb73ec54d0f8
ErikScow <erikscow@gmail.com>
2021-02-24 23:20:10 -0800

    added drafts of authentication flow calls to the front end

---
+++ frontend/src/api/backendCalls.js
@@ -0,0 +1,50 @@
+import axios from 'axios'
+
+const axiosWithAuth = () => {
+    const token = localStorage.getItem('token')
+
+    return axios.create({
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    })
+}
+
+const register = (newUser) => {
+    axios
+    .post(`${process.env.REACT_APP_BACKEND_BASE_URL}/api/auth/register`,newUser)
+    .then(res => {
+        console.log(res)
+    })
+    .catch(err => {
+        console.error(err)
+    })
+}
+
+const login = (loginInfo) => {
+    axios
+    .post(`${process.env.REACT_APP_BACKEND_BASE_URL}/api/auth/login`,loginInfo)
+    .then(res => {
+        console.log(res)
+    })
+    .catch(err => {
+        console.error(err)
+    })
+}
+
+const checkToken = (userId)  => {
+    axiosWithAuth.get(`${process.env.REACT_APP_BACKEND_BASE_URL}/api/auth/${userId}`)
+    .then(res => {
+        console.log(res)
+    })
+    .catch(err => {
+        console.error(err)
+    })
+}
+
+module.exports = {
+    axiosWithAuth,
+    register,
+    login,
+    checkToken
+}
