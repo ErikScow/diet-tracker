a6607d2958e77a4e79af45a841500f8c0e14ea24
Erik Scow <erikscow@gmail.com>
2021-03-08 20:21:07 -0800

    Landing page in development

--- frontend/src/components/welcome/Welcome.js
+++ frontend/src/components/welcome/Welcome.js
@@ -2,10 +2,46 @@ import React from 'react';
 
 import Nav from '../common/Nav'
 
+import { makeStyles } from '@material-ui/core/styles'
+import { Grid, Typography } from '@material-ui/core'
+
+const useStyles = makeStyles((theme) => ({
+    card: {
+        padding: '10px'
+    },
+    image: {
+        width: '90%',
+        margin: '0 auto'
+    },
+    firstBox: {
+        height: '150px',
+        border: '1px solid grey',
+        borderRadius: '10px',
+        padding: '20px',
+        marginTop: '20px'
+    },
+    firstBoxText: {
+        margin: 'auto'
+    }
+  }));
 function Welcome(props) {
+    const classes = useStyles()
+
     return (
         <div>
             <Nav />
+            <Grid container direction='row'>
+                <Grid item xs={1}></Grid>
+                <Grid item container xs={10}>
+                    <Grid item container direction='column' className={classes.firstBox}>
+                        <Typography variant='h5' className={classes.firstBoxText}>Landing Page in development</Typography>
+                    </Grid>
+                    
+                   
+                    
+                </Grid>
+                <Grid item xs={1} md={2}></Grid>
+            </Grid>
         </div>
     );
 }
